288911b8569690858631bf2375d58adf39cd1536
ozaharsh95 <dsouzaaron77@gmail.com>
2026-06-28 12:16:23 +0530

    feat:Implemented new GET route for subjects module
    understand and wrote how drizzle,express with typescript use to build production ready API <3

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import subjectRouter from "./routes/subjects.js";
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+
+app.use("/api/subjects", subjectRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello welcome !!!");
