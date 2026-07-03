ca79b096e783c149f280bc10c74ae0ba9bfcd018
ozaharsh95 <dsouzaaron77@gmail.com>
2026-07-03 17:48:47 +0530

    feat:implemented arcjet middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import subjectRouter from "./routes/subjects.js";
+import securityMiddleware from "./middleware/security.js";
 
 const app = express();
 const PORT = 8000;
@@ -18,6 +19,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(securityMiddleware);
 
 app.use("/api/subjects", subjectRouter);
 
